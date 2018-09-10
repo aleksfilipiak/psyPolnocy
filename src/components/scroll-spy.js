61e768cd054eaf60816b7d4cd714009e5940dfd4
Aleksandra Filipiak <aleksandra.filipiak12@gmail.com>
2018-09-10 10:07:45 +0200

    scroll spy added

--- src/components/scroll-spy.js
+++ src/components/scroll-spy.js
@@ -4,19 +4,63 @@ import React from "react";
 export default class ScrollSpy extends React.Component {
     render() {
         return (
-            <div data-spy="scroll" data-target="#psynav" data-offset="0">
-                <div className="parallax"></div>
+            <div data-spy="scroll" data-target="#psynav" data-offset="0" className="spyScroll">
                 <h4 id="about">O nas</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet tellus dictum elementum tempor. Fusce et sodales metus, id lacinia tortor. Sed id iaculis risus, eget hendrerit urna. Suspendisse neque tellus, ultricies ac lacus id, bibendum varius nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris quis fringilla lectus. Maecenas odio erat, interdum id orci sit amet, facilisis dapibus est. Sed in fermentum magna, non tempus nibh. Phasellus ac mi id mi lobortis hendrerit. Ut eu lobortis lorem. Ut et nisi porttitor, posuere neque id, accumsan sem. Praesent aliquam sem tortor, quis suscipit elit imperdiet et. Cras aliquam tellus lectus, ac tempus lectus cursus vel. Aliquam aliquam vehicula maximus.</p>
+                <p>Naszym celem jest niesienie pomocy pokrzywdzonym i bezdomnym Psom Północy .
+                    wszędzie tam ,gdzie cierpi pies ...
+                    Husky nie lubią samotności, dlatego najlepiej się czują, stale towarzysząc człowiekowi lub
+                    przebywając wśród innych psów (zwłaszcza własnej rasy). Zachowały wiele cech typowych dla wilczych
+                    przodków. Częściej wyją, niż szczekają, a w ich stadzie panuje ścisła hierarchia, co prowadzi czasem
+                    do walk dominacyjnych. Grupa przejawia też silne zachowania terytorialne i może być groźna dla
+                    intruzów.
+                    <br/>
+                    <br/>
+                    Ciekawość, spryt i duża sprawność fizyczna sprawiają, że husky jest mistrzem ucieczek. Jeśli
+                    trzymamy go na zewnątrz, ogród musi otaczać wysokie ogrodzenie (dwumetrowy płot może nie być
+                    wystarczającą przeszkodą dla przedstawiciela tej rasy) na solidnej podmurówce. Nawet największy
+                    ogród nie zastąpi jednak regularnych spacerów i treningów.
+                </p>
                 <div className="parallax"></div>
                 <h4 id="toAddopt">Psy do adopcji</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet tellus dictum elementum tempor. Fusce et sodales metus, id lacinia tortor. Sed id iaculis risus, eget hendrerit urna. Suspendisse neque tellus, ultricies ac lacus id, bibendum varius nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris quis fringilla lectus. Maecenas odio erat, interdum id orci sit amet, facilisis dapibus est. Sed in fermentum magna, non tempus nibh. Phasellus ac mi id mi lobortis hendrerit. Ut eu lobortis lorem. Ut et nisi porttitor, posuere neque id, accumsan sem. Praesent aliquam sem tortor, quis suscipit elit imperdiet et. Cras aliquam tellus lectus, ac tempus lectus cursus vel. Aliquam aliquam vehicula maximus.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet tellus dictum elementum
+                    tempor. Fusce et sodales metus, id lacinia tortor. Sed id iaculis risus, eget hendrerit urna.
+                    Suspendisse neque tellus, ultricies ac lacus id, bibendum varius nunc. Vestibulum ante ipsum primis
+                    in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris quis fringilla lectus. Maecenas
+                    odio erat, interdum id orci sit amet, facilisis dapibus est. Sed in fermentum magna, non tempus
+                    nibh. Phasellus ac mi id mi lobortis hendrerit. Ut eu lobortis lorem. Ut et nisi porttitor, posuere
+                    neque id, accumsan sem. Praesent aliquam sem tortor, quis suscipit elit imperdiet et. Cras aliquam
+                    tellus lectus, ac tempus lectus cursus vel. Aliquam aliquam vehicula maximus.</p>
+                <div className="parallax"></div>
                 <h4 id="docs">Dokumenty do pobrania</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet tellus dictum elementum tempor. Fusce et sodales metus, id lacinia tortor. Sed id iaculis risus, eget hendrerit urna. Suspendisse neque tellus, ultricies ac lacus id, bibendum varius nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris quis fringilla lectus. Maecenas odio erat, interdum id orci sit amet, facilisis dapibus est. Sed in fermentum magna, non tempus nibh. Phasellus ac mi id mi lobortis hendrerit. Ut eu lobortis lorem. Ut et nisi porttitor, posuere neque id, accumsan sem. Praesent aliquam sem tortor, quis suscipit elit imperdiet et. Cras aliquam tellus lectus, ac tempus lectus cursus vel. Aliquam aliquam vehicula maximus.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet tellus dictum elementum
+                    tempor. Fusce et sodales metus, id lacinia tortor. Sed id iaculis risus, eget hendrerit urna.
+                    Suspendisse neque tellus, ultricies ac lacus id, bibendum varius nunc. Vestibulum ante ipsum primis
+                    in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris quis fringilla lectus. Maecenas
+                    odio erat, interdum id orci sit amet, facilisis dapibus est. Sed in fermentum magna, non tempus
+                    nibh. Phasellus ac mi id mi lobortis hendrerit. Ut eu lobortis lorem. Ut et nisi porttitor, posuere
+                    neque id, accumsan sem. Praesent aliquam sem tortor, quis suscipit elit imperdiet et. Cras aliquam
+                    tellus lectus, ac tempus lectus cursus vel. Aliquam aliquam vehicula maximus.</p>
+                <div className="parallax"></div>
                 <h4 id="happyEnds">Szczęśliwe zakończenia</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet tellus dictum elementum tempor. Fusce et sodales metus, id lacinia tortor. Sed id iaculis risus, eget hendrerit urna. Suspendisse neque tellus, ultricies ac lacus id, bibendum varius nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris quis fringilla lectus. Maecenas odio erat, interdum id orci sit amet, facilisis dapibus est. Sed in fermentum magna, non tempus nibh. Phasellus ac mi id mi lobortis hendrerit. Ut eu lobortis lorem. Ut et nisi porttitor, posuere neque id, accumsan sem. Praesent aliquam sem tortor, quis suscipit elit imperdiet et. Cras aliquam tellus lectus, ac tempus lectus cursus vel. Aliquam aliquam vehicula maximus.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet tellus dictum elementum
+                    tempor. Fusce et sodales metus, id lacinia tortor. Sed id iaculis risus, eget hendrerit urna.
+                    Suspendisse neque tellus, ultricies ac lacus id, bibendum varius nunc. Vestibulum ante ipsum primis
+                    in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris quis fringilla lectus. Maecenas
+                    odio erat, interdum id orci sit amet, facilisis dapibus est. Sed in fermentum magna, non tempus
+                    nibh. Phasellus ac mi id mi lobortis hendrerit. Ut eu lobortis lorem. Ut et nisi porttitor, posuere
+                    neque id, accumsan sem. Praesent aliquam sem tortor, quis suscipit elit imperdiet et. Cras aliquam
+                    tellus lectus, ac tempus lectus cursus vel. Aliquam aliquam vehicula maximus.</p>
+                <div className="parallax"></div>
                 <h4 id="rainbowBridge">Za tęczowym mostem</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet tellus dictum elementum tempor. Fusce et sodales metus, id lacinia tortor. Sed id iaculis risus, eget hendrerit urna. Suspendisse neque tellus, ultricies ac lacus id, bibendum varius nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris quis fringilla lectus. Maecenas odio erat, interdum id orci sit amet, facilisis dapibus est. Sed in fermentum magna, non tempus nibh. Phasellus ac mi id mi lobortis hendrerit. Ut eu lobortis lorem. Ut et nisi porttitor, posuere neque id, accumsan sem. Praesent aliquam sem tortor, quis suscipit elit imperdiet et. Cras aliquam tellus lectus, ac tempus lectus cursus vel. Aliquam aliquam vehicula maximus.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet tellus dictum elementum
+                    tempor. Fusce et sodales metus, id lacinia tortor. Sed id iaculis risus, eget hendrerit urna.
+                    Suspendisse neque tellus, ultricies ac lacus id, bibendum varius nunc. Vestibulum ante ipsum primis
+                    in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris quis fringilla lectus. Maecenas
+                    odio erat, interdum id orci sit amet, facilisis dapibus est. Sed in fermentum magna, non tempus
+                    nibh. Phasellus ac mi id mi lobortis hendrerit. Ut eu lobortis lorem. Ut et nisi porttitor, posuere
+                    neque id, accumsan sem. Praesent aliquam sem tortor, quis suscipit elit imperdiet et. Cras aliquam
+                    tellus lectus, ac tempus lectus cursus vel. Aliquam aliquam vehicula maximus.</p>
+                <div className="parallax"></div>
                 <h4 id="contact">Kontakt</h4>
                 <p>Kontakt</p>
             </div>)
