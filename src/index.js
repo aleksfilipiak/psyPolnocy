ac6f46f8aaa9e091af20f457cce59d725a7f6fbd
Aleksandra Filipiak <aleksandra.filipiak12@gmail.com>
2018-09-24 01:11:39 +0200

    change to html instead react

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/app";
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("dokument dziala");
 
-
-ReactDOM.render(
-    <div>
-        <App/>
-    </div>,
-    document.querySelector(".root")
-);
+});
